350302e2efc66d2f037dfb98407f2ecc039f9f47
Lucas Silvério <oliveiratricolorfc@hotmail.com>
2024-06-24 21:39:28 -0300

    Service layer, component registration

--- src/main/java/com/los/course/resources/UserResource.java
+++ src/main/java/com/los/course/resources/UserResource.java
@@ -1,21 +1,28 @@
 package com.los.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.los.course.entities.User;
+import com.los.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource 
 {
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll()
-	{
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll()
+	{	
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
---
+++ src/main/java/com/los/course/services/UserService.java
@@ -0,0 +1,21 @@
+package com.los.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.los.course.entities.User;
+import com.los.course.repositories.UserRepository;
+
+@Service
+public class UserService 
+{
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll()
+	{
+		return repository.findAll();
+	}
+}
