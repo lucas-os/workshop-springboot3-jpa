708019e4ff1add1ae9bf8e71cf7e2f57babc9af7
Lucas Silvério <oliveiratricolorfc@hotmail.com>
2024-06-25 21:26:27 -0300

    Category

---
+++ src/main/java/com/los/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.los.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.los.course.entities.Category;
+import com.los.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}	
