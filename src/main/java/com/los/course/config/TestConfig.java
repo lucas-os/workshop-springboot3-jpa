22a12acbaa39395df1ad8b664851baaf318e535b
Lucas Silvério <oliveiratricolorfc@hotmail.com>
2024-07-10 22:43:17 -0300

    Payment, one-to-one association

--- src/main/java/com/los/course/config/TestConfig.java
+++ src/main/java/com/los/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.los.course.entities.Category;
 import com.los.course.entities.Order;
 import com.los.course.entities.OrderItem;
+import com.los.course.entities.Payment;
 import com.los.course.entities.Product;
 import com.los.course.entities.User;
 import com.los.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
